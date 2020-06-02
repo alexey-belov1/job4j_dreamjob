877a2a9a1cde8ee4e4456fcbecffdb2675477e46
alexey-belov1 <veshousis1@gmail.com>
2020-06-02 20:21:02 +0500

    4. Регистрация пользователя (3)

--- src/main/java/ru/job4j/dream/servlet/RegServlet.java
+++ src/main/java/ru/job4j/dream/servlet/RegServlet.java
@@ -1,6 +1,5 @@
 package ru.job4j.dream.servlet;
 
-import ru.job4j.dream.model.Post;
 import ru.job4j.dream.model.User;
 import ru.job4j.dream.store.PsqlStore;
 
@@ -15,20 +14,14 @@ public class RegServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
-        String email = req.getParameter("email");
-        if (PsqlStore.instOf().findByEmailUser(email) != null) {
-            req.setAttribute("error", "Данный email уже занят");
-            req.getRequestDispatcher("reg.jsp").forward(req, resp);
-        } else {
-            PsqlStore.instOf().save(
-                    new User(
-                            0,
-                            req.getParameter("name"),
-                            email,
-                            req.getParameter("password")
-                    )
-            );
-            req.getRequestDispatcher("login.jsp").forward(req, resp);
-        }
+        PsqlStore.instOf().save(
+                new User(
+                        0,
+                        req.getParameter("name"),
+                        req.getParameter("email"),
+                        req.getParameter("password")
+                )
+        );
+        req.getRequestDispatcher("login.jsp").forward(req, resp);
     }
 }
